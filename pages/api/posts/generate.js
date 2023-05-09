2e1215844cdb99f9f4aebfd5ad8cc8cb6f148423
Theophilus Ighalo <theophilusighalo@gmail.com>
2023-05-09 22:05:29 +0100

    generate

--- pages/api/posts/generate.js
+++ pages/api/posts/generate.js
@@ -10,6 +10,12 @@ const openai = new OpenAIApi(configuration);
 export default async function handler(req, res) {
   try {
     const { user: currentUser } = await getSession(req, res);
+
+    // `await clientPromise` will use the default database passed in the MONGODB_URI
+    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
+    //
+    // `const client = await clientPromise`
+    // `const db = client.db("myDatabase")`
     const client = await clientPromise;
     const db = client.db("blogGen");
 
